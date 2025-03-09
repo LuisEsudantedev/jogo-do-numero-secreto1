82de3b4c5c6f2168eb4ee8f16c03c81c4a499850
LuisEsudantedev <luis_depaula@outlook.com.br>
2025-03-09 07:28:21 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+
+// Importar a biblioteca MaryTTS
+const MaryTTS = require('marytts');
+
+// Criar uma instância da MaryTTS
+const mary = new MaryTTS();
+
+// Definir a língua e a voz
+mary.setLocale('pt-BR');
+mary.setVoice('cmu-slt-hsmm');
+
+// Definir o texto a ser falado
+const texto = 'Olá, bem-vindo ao nosso site!';
+
+// Fazer a MaryTTS falar
+mary.speak(texto, (err) => {
+ if (err) {
+ console.error(err);
+ } else {
+ console.log('Falei!');
+ }
+});
+
+
 alert('essa versão está está sendo atualizado.');
 console.log('Versao em atualização.');
 
